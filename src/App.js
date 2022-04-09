1db02017e8c7f2ed34c279efec059de198252fed
tamagochita <mevere66@gmail.com>
2022-04-09 13:29:45 -0500

    lsitas

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
-import Contador from './components/Contador';
-import Eventos from './components/Eventos';
-import Parrafo from './components/Parrafo'
-import Variables from './components/Variables';
+import Listas from './components/Listas';
+// import Contador from './components/Contador';
+// import Eventos from './components/Eventos';
+// import Parrafo from './components/Parrafo'
+// import Variables from './components/Variables';
 function App() {
   return (
     <div>
       <h1>Hola mundo react</h1>
-      <Parrafo />
+      {/* <Parrafo />
       <Variables />
       <Eventos />
       <hr>
       </hr>
-      <Contador/>
+      <Contador/> */}
+      <Listas />
+
+
     </div>
   );
 }
