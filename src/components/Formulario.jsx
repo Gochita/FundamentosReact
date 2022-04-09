9b82a20a1e4db89ef7442272560211734a8acda5
tamagochita <mevere66@gmail.com>
2022-04-09 16:08:46 -0500

    prueba formularios

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,30 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Formulario = () => {
 
-    const [fruta,setFruta]=React.useState('');
-    const [descripcion,setDescripcion]=React.useState('');
+    const [fruta, setFruta] = React.useState('');
+    const [descripcion, setDescripcion] = React.useState('');
+    const [lista, setLista] = React.useState([]);
 
-  return (
-    <div className='container mt-5'>
-        <h2>formulario</h2>
-        <form>
-            <input 
-            type="text" 
-            placeholder='Ingrese fruta'
-            className='form-control mb-2'
-            onChange={(e) => setFruta(e.target.value) }
-            />
-             <input 
-            type="text" 
-            placeholder='Ingrese descripcion'
-            className='form-control mb-2'
-            onChange={(e) => setDescripcion(e.target.value) }
-            />
-            <button className="btn btn-primary btn-block">Agregar</button>
-        </form>
-    </div>
-  )
+
+
+    const guardarDatos = (e) => {
+        e.preventDefault()
+
+        if (!fruta.trim()) {
+            console.log('esta vacio fruta')
+            return
+        }
+
+        if (!descripcion.trim()) {
+            console.log('esta vacio descripcion')
+            return
+        }
+
+        console.log('procesando datos...' + fruta + descripcion)
+
+        setLista([
+            ...lista,
+            { nombraFruta: fruta, nombreDescripcion: descripcion }
+        ])
+
+        e.target.reset()
+        setFruta('')
+        setDescripcion('')
+    }
+
+    return (
+        <div className='container mt-5'>
+            <h2>formulario</h2>
+            <form onSubmit={guardarDatos}>
+                <input
+                    type="text"
+                    placeholder='Ingrese fruta'
+                    className='form-control mb-2'
+                    onChange={(e) => setFruta(e.target.value)}
+                />
+                <input
+                    type="text"
+                    placeholder='Ingrese descripcion'
+                    className='form-control mb-2'
+                    onChange={(e) => setDescripcion(e.target.value)}
+                />
+                <ul>
+                    {
+                        lista.map((item, index) => (
+                            <li key={index}>
+                                {item.nombraFruta} - {item.nombreDescripcion}
+                            </li>
+                        ))
+                    }
+                </ul>
+                <button
+                    className="btn btn-primary btn-block"
+                    type='submit'
+                >Agregar</button>
+
+
+            </form>
+        </div>
+    )
 }
 
 export default Formulario
